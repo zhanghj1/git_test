1e7e9fdd342c685908178af35d6766ba4f0fdca6
zhanghj1 <192279992@qq.com>
2020-07-05 13:52:47 +0800

    test branch dev_0705

--- src/main/java/com/zhanghj/java_se_demo/io/FileTest.java
+++ src/main/java/com/zhanghj/java_se_demo/io/FileTest.java
@@ -9,6 +9,7 @@ import java.util.Date;
  */
 public class FileTest {
     public static void main(String[] args) {
+        System.out.println("test branch 0705_dev");
         File file = new File("F:\\study\\nio");
         printSubFile(file);
     }
